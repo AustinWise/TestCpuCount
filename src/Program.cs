5811b8338bcba340d3c6383a4616c1f6325aeb9a
Austin Wise <AustinWise@gmail.com>
2024-02-28 19:24:47 +0000

    Print lscpu info

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,36 @@
+using System.Diagnostics;
 using System.Text;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 var app = builder.Build();
 
-var sb = new StringBuilder();
-
-app.MapGet("/", () => "Hi from .NET");
+app.MapGet("/", () => GetInfo());
 
 app.Run();
+
+static string GetInfo()
+{
+    var sb = new StringBuilder();
+    sb.AppendLine($"CPU Count: {Environment.ProcessorCount}");
+    sb.AppendLine();
+
+    try
+    {
+        var psi = new ProcessStartInfo("lscpu")
+        {
+            RedirectStandardOutput = true,
+        };
+
+        var p = Process.Start(psi);
+        p.WaitForExit();
+        var output = p.StandardOutput.ReadToEnd();
+        sb.AppendLine(output);
+    }
+    catch (Exception ex)
+    {
+        sb.AppendLine("Failed to start lscpu:");
+        sb.AppendLine(ex.ToString());
+    }
+    return sb.ToString();
+}
